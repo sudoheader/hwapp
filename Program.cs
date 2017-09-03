d8745324d8ef5b89cdb770e37260ada424974b98
sudoheader <sudoheader@users.noreply.github.com>
2017-09-02 23:57:40 -0700

    108. Math

--- Program.cs
+++ Program.cs
@@ -7,11 +7,13 @@ namespace hwapp
         public static void Main(string[] args)
         {
             int numbers = 10;
+            int additionVar = 3;
             string letters = "Letters 1234";
             bool trueFalse = true;
 
-            Console.WriteLine(trueFalse);
             Console.WriteLine(numbers);
+            Console.WriteLine(additionVar);
+            Console.WriteLine(numbers / additionVar);
         }
     }
 }
