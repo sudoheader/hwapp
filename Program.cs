2fb93d4163f48a9374610b9a786ea61b87d75e16
sudoheader <sudoheader@users.noreply.github.com>
2017-09-03 19:09:30 -0700

    111. Math outside of the console

--- Program.cs
+++ Program.cs
@@ -9,10 +9,14 @@ namespace hwapp
             int score = 10;
             int lives = 3;
             double decimalPlaces = 3.5;
+            int answer;
             string letters = "Letters 1234";
             bool trueFalse = true;
 
-            Console.WriteLine("Score: " + score + " " + "Lives: " + lives);
+            // Math
+            answer = score + lives + 1;
+
+            Console.WriteLine(answer);
         }
     }
 }
