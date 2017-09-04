e6533f27dc0330fd7ff2240ae86c43ef8356b049
sudoheader <sudoheader@users.noreply.github.com>
2017-09-04 15:42:27 -0700

    112. Variable scope

--- Program.cs
+++ Program.cs
@@ -8,15 +8,15 @@ namespace hwapp
         {
             int score = 10;
             int lives = 3;
-            double decimalPlaces = 3.5;
             int answer;
-            string letters = "Letters 1234";
-            bool trueFalse = true;
 
             // Math
             answer = score + lives + 1;
 
             Console.WriteLine(answer);
+        }
+        public static void Awesome() {
+            Console.WriteLine(score);
         }
     }
 }
