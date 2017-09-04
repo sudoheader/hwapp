13169d2ed51440cc16dcf47e0c0a371bcaa65111
sudoheader <sudoheader@users.noreply.github.com>
2017-09-04 15:48:02 -0700

    114. Passing variables to voids

--- Program.cs
+++ Program.cs
@@ -9,15 +9,18 @@ namespace hwapp
         {
             int score = 10;
             int lives = 3;
-            int answer;
+            int answer = 4;
 
             // Math
             answer = score + lives + 1;
 
-            Awesome();
+            Awesome(answer);
         }
-        public static void Awesome() {
-            Console.WriteLine(GlobalScore);
+        public static void Awesome(int a) {
+            Console.WriteLine(GlobalScore + a);
+        }
+        public static void addition(int b) {
+            Console.WriteLine(b);
         }
     }
 }
